71a4dc65db0e13ac575f40ffc783b7dae978cb32
Deepashri Dabhade <89677697+Deepashri22@users.noreply.github.com>
2024-03-23 22:46:29 +0530

    Create Remove Duplicates from Sorted List.java

---
+++ Remove Duplicates from Sorted List.java
@@ -0,0 +1,33 @@
+/* Linked List Node
+struct Node {
+    int data;
+    struct Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}; */
+
+class Solution {
+    Node removeDuplicates(Node head) {
+        // your code here
+        HashSet<Integer> hs = new HashSet<>(); 
+        Node curr=head;
+        Node prev=null;
+         
+        while(curr!=null){
+            int val = curr.data;
+            if (hs.contains(val))
+                prev.next=curr.next;
+                    
+            else{
+                hs.add(val);
+                prev=curr;
+            }
+                 
+            curr=curr.next;
+        }
+       
+        return head;
+    }
+};
